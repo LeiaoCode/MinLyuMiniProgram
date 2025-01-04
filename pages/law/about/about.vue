deace067e382fa123c24ae61a4f376b1b89953a0
LeiaoCode <925932536@qq.com>
2025-01-04 22:06:42 +0800

    2025年1月4日22:06:40

---
+++ pages/law/about/about.vue
@@ -0,0 +1,102 @@
+<template>
+	<!--本文件由FirstUI授权予向*磊（会员ID：   3789）专用，请尊重知识产权，勿私下传播，违者追究法律责任。-->
+	<view class="fui-wrap">
+		<view class="fui-logo__box">
+			<image src="/static/images/law/logo.jpg" class="fui-logo" mode="widthFix"></image>
+			<view class="fui-title">民律在线</view>
+			<view class="fui-version">Version 5.2.1</view>
+		</view>
+
+		<view class="fui-page__bd">
+			<view class="fui-desc">
+				<text class="fui-mr">民律在线</text> 是一款专注于法律咨询的高效小程序，致力于为用户提供便捷、专业的法律服务。无论是日常法律问题，还是专业的法律诉讼问题，民律在线 都能为您提供及时、准确的解答。通过集成的法律知识库和专家在线服务，用户可以快速获得法律咨询，避免复杂的法律程序，提升问题解决效率。
+			</view>
+<!-- 			<fui-list-cell topBorder arrow @click="getLink">
+				去STAR
+			</fui-list-cell>
+			<fui-list-cell arrow @click="href('../updated/updated')">
+				功能介绍
+			</fui-list-cell>
+			<fui-list-cell bottomLeft="0" arrow @click="href('../vip/vip')">
+				会员介绍
+			</fui-list-cell> -->
+		</view>
+	</view>
+</template>
+
+<script>
+	import {
+		mapState
+	} from 'vuex'
+	import $fui from '@/components/firstui/fui-clipboard';
+	export default {
+		computed: mapState(['version','status']),
+		data() {
+			return {
+
+			}
+		},
+		methods: {
+			getLink(e) {
+				// #ifdef MP-BAIDU
+				 if(this.status==0){
+					 this.fui.toast('暂不支持此功能~')
+					 return;
+				 }
+				// #endif
+				$fui.getClipboardData('https://github.com/FirstUI/FirstUI', res => {
+					this.fui.toast('GitHub地址复制成功');
+				}, e);
+			},
+			href(page){
+				this.fui.href(page)
+			}
+		}
+	}
+</script>
+
+<style>
+	page {
+		background-color: #fff;
+		font-weight: normal;
+	}
+
+	.fui-logo__box {
+		width: 100%;
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		justify-content: center;
+		padding-top: 64rpx;
+		padding-bottom: 64rpx;
+	}
+
+	.fui-logo {
+		width: 120rpx;
+		height: 120rpx;
+		border-radius: 24rpx;
+	}
+
+	.fui-title {
+		font-size: 36rpx;
+		padding-top: 24rpx;
+	}
+
+	.fui-version {
+		padding-top: 8rpx;
+		font-size: 24rpx;
+		font-weight: 400;
+	}
+	.fui-desc {
+		font-size: 28rpx;
+		line-height: 56rpx;
+		padding: 0 32rpx;
+		margin-bottom: 64rpx;
+		box-sizing: border-box;
+		font-weight: 400;
+	}
+
+	.fui-mr {
+		margin-right: 8rpx;
+	}
+</style>
---
+++ pages/law/skeleton/skeleton.vue
@@ -0,0 +1,163 @@
+<template>
+	<view class="fui-wrap">
+		<view class="fui-page__bd">
+			<view class="fui-sk__wrap fui-round">骨架节点</view>
+		</view>
+		<view class="fui-page__bd fui-page__spacing">
+			<view class="fui-section__title">擅长类型</view>
+			<view class="fui-align__center">
+				<fui-tag text="标签一" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签二" type="danger" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签三" type="success" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签四" type="warning" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签四" type="warning" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签四" type="warning" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签四" type="warning" margin-bottom="24" margin-right="24"></fui-tag>
+				<fui-tag text="标签五" type="purple" margin-bottom="24"></fui-tag>
+			</view>
+			<view class="fui-section__title">律师简介</view>
+			<fui-alert type="warn" isLeft spacing title="An info alert" size="28rpx" :marginTop="24">
+				<fui-icon name="evaluate" :size="48" color="#fff"></fui-icon>
+			</fui-alert>
+			<view class="fui-section__title">律师位置</view>
+			<fui-alert type="warn" isLeft spacing title="An info alert" size="28rpx" :marginTop="24">
+				<fui-icon name="location-fill" :size="48" color="#fff"></fui-icon>
+			</fui-alert>
+			<fui-divider backgroundColor="#fff" dividerColor="#F1F4FA" color="#465CFF" text="联系方式" :size="28"></fui-divider>
+			<view class="fui-page__bd">
+				<view class="fui-contacts__box">
+					<view class="fui-contacts__item" v-for="(item,index) in contacts" :key="index"
+						@tap="copy($event,item.value,item.text)">
+						<image class="fui-icon" :src="`/static/images/cooperate/light/icon_${item.icon}_3x.png`"></image>
+						<view class="fui-title">{{item.text}}</view>
+					</view>
+				</view>
+			</view>
+		</view>
+
+	</view>
+</template>
+
+<script>
+	import $fui from '@/components/firstui/fui-clipboard';
+	export default {
+		data() {
+			return {
+				show: true,
+				contacts: [ {
+					icon: 'douyin',
+					text: '抖音',
+					value: '1223213'
+				},{
+					icon: 'weixin',
+					text: '微信',
+					value: 'wx_firstui'
+				}, {
+					icon: 'qq',
+					text: 'QQ',
+					value: '3560662983'
+				},{
+					icon: 'email',
+					text: '邮箱',
+					value: 'firstui@126.com'
+				}]
+			}
+		},
+		onReady() {
+			setTimeout(() => {
+				this.show = false
+			}, 2000)
+		},
+		methods: {
+			copy(e, content, title) {
+				title = title || '链接'
+				$fui.getClipboardData(content, res => {
+					this.fui.toast(`${title}复制成功`);
+				}, e);
+			}
+		}
+	}
+</script>
+
+<style>
+	.fui-sk__wrap {
+		background: #16AB60;
+		color: #fff;
+		width: 200rpx;
+		height: 200rpx;
+		border-radius: 50%;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		margin: 40rpx auto;
+	}
+
+	.fui-section__title {
+		margin-left: 32rpx;
+		margin-top: 34rpx;
+	}
+
+	.fui-align__center {
+		flex-wrap: wrap;
+	}
+
+	.fui-close__icon {
+		display: flex;
+		align-items: center;
+		padding: 6rpx 0 4rpx 24rpx;
+	}
+	
+	/* 联系方式 */
+	
+	.fui-page__bd {
+		background-color: #fff;
+	}
+	
+	.fui-menu__icon {
+		width: 96rpx;
+		height: 96rpx;
+		display: block;
+		margin-bottom: 16rpx;
+	}
+	
+	.fui-contacts__box {
+		width: 100%;
+		padding: 32rpx 32rpx 20rpx;
+		box-sizing: border-box;
+		display: flex;
+		align-items: center;
+	}
+	
+	.fui-contacts__item {
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+		flex: 1;
+		/* #ifdef H5 */
+		cursor: pointer;
+		/* #endif */
+	}
+	
+	.fui-icon {
+		width: 96rpx;
+		height: 96rpx;
+	}
+	
+	.fui-title {
+		font-size: 28rpx;
+		color: #333333;
+		padding-top: 16rpx;
+	}
+	
+	.fui-desc {
+		font-size: 24rpx;
+		font-weight: 400;
+		color: #7F7F7F;
+		line-height: 48rpx;
+		width: 100%;
+		padding: 0 64rpx;
+		box-sizing: border-box;
+		text-align: center;
+	}
+</style>
